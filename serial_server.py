c45a6e31086af1c62d37fe785ac9feb36aef9d21
hbh112233abc <hbh112233abc@163.com>
2021-09-24 21:43:31 +0800

    读取到\r,防断片

--- serial_server.py
+++ serial_server.py
@@ -11,7 +11,7 @@ from util import *
 
 
 class SerialServer(threading.Thread):
-    def __init__(self, port, ptl=9600, timeout=600):
+    def __init__(self, port, ptl=9600, timeout=10):
         super().__init__()
         self.port = port
         self.ptl = 9600
@@ -23,32 +23,30 @@ class SerialServer(threading.Thread):
             logger.info(f'start serial server port:{self.port}')
             serials[self.port] = server
             while True:
-                if server.in_waiting:
-                    print('bytes length:', server.in_waiting)
-                    try:
-                        bytes = server.read(server.in_waiting)
-                        logger.info(f'receive qrcode bytes:{bytes}')
-                        char_info = chardet.detect(bytes)
-                        print(char_info)
-                        if int(char_info.get('confidence', 0)) > 0.8:
-                            char_code = char_info.get('encoding')
-                            data = bytes.decode(char_code)
-                        else:
-                            try:
-                                data = bytes.decode('utf-8')
-                            except Exception as e:
-                                print(e)
-                                data = bytes.decode('gbk')
+                try:
+                    bytes = server.read_until(b'\r')
+                    logger.info(f'receive qrcode bytes:{bytes}')
+                    char_info = chardet.detect(bytes)
+                    print(char_info)
+                    if int(char_info.get('confidence', 0)) > 0.8:
+                        char_code = char_info.get('encoding')
+                        data = bytes.decode(char_code)
+                    else:
+                        try:
+                            data = bytes.decode('utf-8')
+                        except Exception as e:
+                            print(e)
+                            data = bytes.decode('gbk')
 
-                        logger.info(f'serial [{self.port}] received:{data}')
-                        msg = {
-                            'action': 'scan_qrcode',
-                            'port': self.port,
-                            'data': data,
-                        }
-                        websocket_notify(json.dumps(msg))
-                    except Exception as e:
-                        logger.exception(e)
+                    logger.info(f'serial [{self.port}] received:{data}')
+                    msg = {
+                        'action': 'scan_qrcode',
+                        'port': self.port,
+                        'data': data,
+                    }
+                    websocket_notify(json.dumps(msg))
+                except Exception as e:
+                    logger.exception(e)
 
         except Exception as e:
             logger.error(e)
