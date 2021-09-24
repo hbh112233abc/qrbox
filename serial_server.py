6c6bf525d9f67c7a955c9e4d4707a2ad25ee19e5
hbh112233abc <hbh112233abc@163.com>
2021-09-24 20:53:50 +0800

    加入编码检测,utf-8解码识别尝试gbk解码

--- serial_server.py
+++ serial_server.py
@@ -4,12 +4,14 @@ import json
 import serial
 import serial.tools.list_ports
 
+import chardet
+
 from init import *
 from util import *
 
 
 class SerialServer(threading.Thread):
-    def __init__(self, port, ptl=9600, timeout=60):
+    def __init__(self, port, ptl=9600, timeout=600):
         super().__init__()
         self.port = port
         self.ptl = 9600
@@ -22,14 +24,31 @@ class SerialServer(threading.Thread):
             serials[self.port] = server
             while True:
                 if server.in_waiting:
-                    data = server.read(server.in_waiting).decode('utf-8')
-                    logger.info(f'serial [{self.port}] received:{data}')
-                    msg = {
-                        'action': 'scan_qrcode',
-                        'port': self.port,
-                        'data': data,
-                    }
-                    websocket_notify(json.dumps(msg))
+                    print('bytes length:', server.in_waiting)
+                    try:
+                        bytes = server.read(server.in_waiting)
+                        logger.info(f'receive qrcode bytes:{bytes}')
+                        char_info = chardet.detect(bytes)
+                        print(char_info)
+                        if int(char_info.get('confidence', 0)) > 0.8:
+                            char_code = char_info.get('encoding')
+                            data = bytes.decode(char_code)
+                        else:
+                            try:
+                                data = bytes.decode('utf-8')
+                            except Exception as e:
+                                print(e)
+                                data = bytes.decode('gbk')
+
+                        logger.info(f'serial [{self.port}] received:{data}')
+                        msg = {
+                            'action': 'scan_qrcode',
+                            'port': self.port,
+                            'data': data,
+                        }
+                        websocket_notify(json.dumps(msg))
+                    except Exception as e:
+                        logger.exception(e)
 
         except Exception as e:
             logger.error(e)
